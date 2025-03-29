9a86b14b34581f00ceed347edfa54cbfb532f8e0
Drjeey <jeremykasule22@gmail.com>
2025-03-29 18:02:28 +0300

    Replace chatbot_system.sql with a new database.sql file containing updated table structures and initial data for chats and users.

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 29, 2025 at 03:28 PM
+-- Generation Time: Mar 29, 2025 at 04:01 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `chatbot_system`
+-- Database: `database`
 --
 
 -- --------------------------------------------------------
@@ -36,6 +36,38 @@ CREATE TABLE `chats` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `chats`
+--
+
+INSERT INTO `chats` (`id`, `user_id`, `date`, `user_message`, `bot_message`) VALUES
+(7, 5, '2025-03-28 21:00:00', 'hello', 'Hello there! How can I help you today?\n'),
+(8, 5, '2025-03-28 21:00:00', 'everything ok', 'That\'s great to hear! Is there anything I can help you with today?\n');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'mark', '$2y$10$SLNMy857384wSmzN2bkn5u3NIrYcI43607kSQkpkfNL/3Idk2PVyC'),
+(2, 'john', '$2y$10$NYpSMGkCHfCW6ybI/zd8oOHRmgaHJLS4.4pYtPpKwE9ox9nXnnKfO'),
+(3, 'ben', '$2y$10$hNpqgJaM19LjbFHGJM6vA.CnvU61pjfvZLmLhL32ICiesgEH/jCpy'),
+(4, 'Brian', '$2y$10$Ih5ljH3C1MeGEQy52yKEM.r0945GAx55e7cJlzzDsIkghySN85Zvq'),
+(5, 'kasule', '$2y$10$Bq3mALvElvgq1jpJBPSijeW/4bZDMr/pLsIbfrZCPUbiHaFbrlKUW'),
+(6, 'home', '$2y$10$oDat0xdEhExXC6Hpm0M2We/8MCV4o49G3lFGGuh8zjXaniddRfB3K');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -47,6 +79,12 @@ ALTER TABLE `chats`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -54,6 +92,12 @@ ALTER TABLE `chats`
 -- AUTO_INCREMENT for table `chats`
 --
 ALTER TABLE `chats`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
